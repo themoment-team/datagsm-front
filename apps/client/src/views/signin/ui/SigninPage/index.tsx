276555a27052fca33fd1ba601673158a67914543
junjuny0227 <junjuny0227@gmail.com>
2025-10-28 18:11:01 +0900

    feat: 클라이언트 Google OAuth 로그인 기능

--- apps/client/src/views/signin/ui/SigninPage/index.tsx
+++ apps/client/src/views/signin/ui/SigninPage/index.tsx
@@ -2,15 +2,19 @@
 
 import { SigninForm } from '@repo/shared/ui';
 
+import { useGoogleLogin } from '@/features/auth';
+
 const SigninPage = () => {
+  const handleGoogleLogin = useGoogleLogin();
+
   return (
-    <SigninForm
-      title="Data GSM"
-      subtitle="@gsm.hs.kr 도메인 계정만 사용 가능합니다"
-      onGoogleLogin={() => {
-        console.log('client: google login click');
-      }}
-    />
+    <div className="bg-background flex min-h-screen items-center justify-center px-4">
+      <SigninForm
+        title="Data GSM"
+        subtitle="@gsm.hs.kr 계정만 사용 가능합니다"
+        onGoogleLogin={handleGoogleLogin}
+      />
+    </div>
   );
 };
 
