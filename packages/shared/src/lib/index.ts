89e4732af026dc4ef0c337a8ba7a4387e30a3942
junjuny0227 <junjuny0227@gmail.com>
2025-10-28 16:36:51 +0900

    feat: 쿠키 유틸리티 함수 추가

--- packages/shared/src/lib/index.ts
+++ packages/shared/src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './cn';
 export * from './axios';
+export * from './cookies';
 export { default as TanStackProvider } from './TanStackProvider';
 export { default as ToastProvider } from './ToastProvider';
