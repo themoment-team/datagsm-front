e4c06411ae38657498b45625d1e33d85e69c5b4b
junjuny0227 <junjuny0227@gmail.com>
2025-11-27 23:03:46 +0900

    feat: API 명세 페이지 추가

---
+++ apps/client/src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+import { DocsPage } from '@/views/docs';
+
+const Docs = () => {
+  return <DocsPage />;
+};
+
+export default Docs;
---
+++ apps/client/src/views/docs/ui/DocsPage/index.tsx
@@ -0,0 +1,7 @@
+import { cn } from '@repo/shared/utils';
+
+const DocsPage = async () => {
+  return <div className={cn('bg-background min-h-[calc(100vh-4.0625rem)]')}></div>;
+};
+
+export default DocsPage;
