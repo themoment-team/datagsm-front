1331c15c48516581e6d10fdc285961a04afd1352
junjuny0227 <junjuny0227@gmail.com>
2025-12-11 10:18:20 +0900

    refactor: 학생 추가 스키마 유효성 검사 메시지 형식 변경

--- apps/admin/src/entities/student/model/schema.ts
+++ apps/admin/src/entities/student/model/schema.ts
@@ -11,20 +11,22 @@ export const StudentFilterSchema = z.object({
 export type StudentFilterType = z.infer<typeof StudentFilterSchema>;
 
 export const AddStudentSchema = z.object({
-  name: z.string().min(1, '이름을 입력해주세요.'),
-  sex: z.enum(['MAN', 'WOMAN'], '성별을 선택해주세요.'),
-  email: z.email('올바른 이메일 형식이 아닙니다.'),
-  grade: z.number('학년을 선택해주세요.'),
-  classNum: z.number('반을 선택해주세요.'),
-  number: z.number('번호를 선택해주세요.'),
-  role: z.enum(['GENERAL_STUDENT', 'STUDENT_COUNCIL', 'DORMITORY_MANAGER'], '구분을 선택해주세요.'),
+  name: z.string().min(1, { message: '이름을 입력해주세요.' }),
+  sex: z.enum(['MAN', 'WOMAN'], { message: '성별을 선택해주세요.' }),
+  email: z.email({ message: '올바른 이메일 형식이 아닙니다.' }),
+  grade: z.number({ message: '학년을 선택해주세요.' }),
+  classNum: z.number({ message: '반을 선택해주세요.' }),
+  number: z.number({ message: '번호를 선택해주세요.' }),
+  role: z.enum(['GENERAL_STUDENT', 'STUDENT_COUNCIL', 'DORMITORY_MANAGER'], {
+    message: '구분을 선택해주세요.',
+  }),
   dormitoryRoomNumber: z
-    .number('호실을 입력해주세요.')
-    .min(201, '201호 이상으로 입력해주세요.')
-    .max(518, '518호 이하로 입력해주세요.'),
-  majorClubId: z.number().min(1).nullable(),
-  jobClubId: z.number().min(1).nullable(),
-  autonomousClubId: z.number().min(1).nullable(),
+    .number({ message: '호실을 입력해주세요.' })
+    .min(201, { message: '201호 이상으로 입력해주세요.' })
+    .max(518, { message: '518호 이하로 입력해주세요.' }),
+  majorClubId: z.number({ message: '전공 동아리를 선택해주세요.' }).min(1).nullable(),
+  jobClubId: z.number({ message: '취업 동아리를 선택해주세요.' }).min(1).nullable(),
+  autonomousClubId: z.number({ message: '자율 동아리를 선택해주세요.' }).min(1).nullable(),
 });
 
 export type AddStudentType = z.infer<typeof AddStudentSchema>;
