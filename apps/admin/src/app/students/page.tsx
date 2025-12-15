968c605cc21aa30ffa2eb43cf472e3c127603c27
junjuny0227 <junjuny0227@gmail.com>
2025-12-15 14:20:02 +0900

    refactor: 학생 페이지에 Suspense 적용

--- apps/admin/src/app/students/page.tsx
+++ apps/admin/src/app/students/page.tsx
@@ -1,10 +1,16 @@
+import { Suspense } from 'react';
+
 import { getClubs } from '@/entities/club';
 import { StudentsPage } from '@/views/students';
 
 const Students = async () => {
   const [initialClubsData] = await Promise.all([getClubs()]);
 
-  return <StudentsPage initialClubsData={initialClubsData} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <StudentsPage initialClubsData={initialClubsData} />
+    </Suspense>
+  );
 };
 
 export default Students;
