5dcc59e14dab338d1db74f4b56c3d72a8f5cf2f3
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-12-19 08:52:01 +0900

    feat: 타입 선언

--- apps/client/src/entities/home/model/types.ts
+++ apps/client/src/entities/home/model/types.ts
@@ -1,9 +1,34 @@
 import { ApiResponse } from '@repo/shared/types';
+import z from 'zod';
 
 export interface ApiKeyData {
   apiKey: string;
   expiresAt: string;
+  scopes: string[];
+  description: string;
 }
+
+export const CreateApiKeySchema = z.object({
+  scopes: z.array(z.string()).min(1, { message: '권한 범위를 최소 1개 이상 선택해주세요.' }), // 추후 수정
+  description: z.string().min(1, { message: '설명을 입력해주세요.' }),
+});
+
+export type CreateApiKeyType = z.infer<typeof CreateApiKeySchema>;
+
+export interface AvailableScopeData {
+  scope: string;
+  description: string;
+}
+
+export interface AvailableScopeResponse {
+  title: string;
+  description: string;
+  scopes: AvailableScopeData[];
+}
+
+export type getAvailableScopeResponse = ApiResponse<{
+  data: AvailableScopeResponse[];
+}>;
 
 export interface ApiKeyRenewableData {
   renewable: boolean;
