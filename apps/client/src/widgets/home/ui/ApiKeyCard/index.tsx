497ca6c7ea524e42387d1a04ada60e489daf12e5
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-12-19 08:57:01 +0900

    feat: scope 선택 기능 제작

--- apps/client/src/widgets/home/ui/ApiKeyCard/index.tsx
+++ apps/client/src/widgets/home/ui/ApiKeyCard/index.tsx
@@ -1,19 +1,28 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { authQueryKeys } from '@repo/shared/api';
-import { Button, Card } from '@repo/shared/ui';
+import { Button, Card, Checkbox, FormErrorMessage } from '@repo/shared/ui';
 import { cn } from '@repo/shared/utils';
 import { useQueryClient } from '@tanstack/react-query';
 import { Check, Copy, RefreshCw } from 'lucide-react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 
-import { ApiKeyRenewableResponse, ApiKeyResponse } from '@/entities/home';
+import {
+  ApiKeyRenewableResponse,
+  ApiKeyResponse,
+  AvailableScopeData,
+  CreateApiKeySchema,
+  CreateApiKeyType,
+} from '@/entities/home';
 import {
   useCreateApiKey,
   useGetApiKey,
   useGetApiKeyRenewable,
+  useGetAvailableScope,
   useUpdateApiKey,
 } from '@/widgets/home';
 
@@ -26,8 +35,26 @@ const COPIED_STATE_DURATION_MS = 2000;
 
 const ApiKeyCard = ({ initialApiKeyData, initialApiKeyRenewableData }: ApiKeyCardProps) => {
   const [copied, setCopied] = useState(false);
+  const [selectedScopes, setSelectedScopes] = useState<string[]>([]);
 
   const queryClient = useQueryClient();
+
+  const {
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<CreateApiKeyType>({
+    resolver: zodResolver(CreateApiKeySchema),
+    defaultValues: {
+      scopes: [],
+      description: '',
+    },
+  });
+
+  const userRole = 'USER';
+
+  const { data: availableKeyScope, isLoading: isLoadingKeyScope } = useGetAvailableScope(userRole);
 
   const { data: apiKeyData, isLoading: isLoadingApiKey } = useGetApiKey({
     initialData: initialApiKeyData,
@@ -58,6 +85,14 @@ const ApiKeyCard = ({ initialApiKeyData, initialApiKeyRenewableData }: ApiKeyCar
     },
   });
 
+  const buttonText = isCreatingApiKey
+    ? 'API 키 생성 중...'
+    : apiKeyData?.data?.apiKey
+      ? 'API 키 갱신하기'
+      : isUpdatingApiKey
+        ? 'API 키 갱신 중...'
+        : 'API 키 생성하기';
+
   const handleRenew = () => {
     const renewable = apiKeyRenewableData?.data?.renewable;
     if (!renewable) {
@@ -74,7 +109,47 @@ const ApiKeyCard = ({ initialApiKeyData, initialApiKeyRenewableData }: ApiKeyCar
     setTimeout(() => setCopied(false), COPIED_STATE_DURATION_MS);
   };
 
-  if (isLoadingApiKey) {
+  const handleScopeToggle = (scopeId: string) => {
+    // 전체 scope 선택했을때 : ex) student:*
+    // if (scopeId.endsWith(':*')) {
+    //   setSelectedScopes((prev) => {
+    //     const [prefix] = scopeId.split(':');
+    //     const relatedScopeIds = availableKeyScope.flatMap((category) =>
+    //       category.scopes.map((scope) => scope.id).filter((id) => id.startsWith(`${prefix}:`)),
+    //     );
+
+    //     const allSelected = relatedScopeIds.every((id) => prev.includes(id));
+    //     const nextSelected = allSelected
+    //       ? prev.filter((id) => !relatedScopeIds.includes(id))
+    //       : Array.from(new Set([...prev, ...relatedScopeIds]));
+
+    //     setValue('scopes', nextSelected, { shouldValidate: true });
+    //     return nextSelected;
+    //   });
+    //   return;
+    // }
+
+    // 일반 scope 단일 토글
+    setSelectedScopes((prev) => {
+      const isSelected = prev.includes(scopeId);
+      const nextSelected = isSelected ? prev.filter((id) => id !== scopeId) : [...prev, scopeId];
+
+      setValue('scopes', nextSelected, { shouldValidate: true });
+      return nextSelected;
+    });
+  };
+
+  const getIndentation = (level: string) => {
+    if (level.includes(':*')) return 'pl-0';
+    return 'pl-6';
+  };
+
+  const onSubmit = () => {
+    console.log();
+    createApiKey({ scopes: watch('scopes'), description: watch('description') });
+  };
+
+  if (isLoadingApiKey || isLoadingKeyScope) {
     return (
       <Card className={cn('p-6')}>
         <div className={cn('text-gray-500')}>API Key를 불러오는 중...</div>
@@ -82,56 +157,119 @@ const ApiKeyCard = ({ initialApiKeyData, initialApiKeyRenewableData }: ApiKeyCar
     );
   }
 
-  if (!apiKeyData?.data?.apiKey) {
-    return (
-      <Card className={cn('p-6')}>
-        <div className={cn('flex flex-col items-center justify-center gap-4')}>
-          <div className={cn('text-center')}>
-            <p className={cn('mb-2 text-gray-500')}>API Key가 존재하지 않습니다.</p>
-            <p className={cn('text-sm text-gray-400')}>
-              아래 버튼을 눌러 새로운 API Key를 생성하세요.
-            </p>
-          </div>
-          <Button onClick={() => createApiKey()} disabled={isCreatingApiKey} size="lg">
-            {isCreatingApiKey ? 'API Key 생성 중...' : 'API Key 생성하기'}
-          </Button>
-        </div>
-      </Card>
-    );
-  }
+  // if (isLoadingKeyScope) {
+  //   return (
+  //     <Card className={cn('p-6')}>
+  //       <div className={cn('text-gray-500')}>API key 범위를 불러오는 중...</div>
+  //     </Card>
+  //   );
+  // }
 
   return (
-    <Card className={cn('p-6')}>
-      <div className={cn('flex items-center justify-between gap-4')}>
-        <div className={cn('min-w-0 flex-1')}>
-          <code className={cn('break-all font-mono text-sm')}>{apiKeyData.data.apiKey}</code>
-        </div>
-        <div className={cn('flex gap-2')}>
-          <Button
-            size="icon"
-            variant="outline"
-            onClick={handleRenew}
-            disabled={isUpdatingApiKey}
-            className={cn('disabled:opacity-100')}
-          >
-            <RefreshCw className={cn(`h-4 w-4 ${isUpdatingApiKey ? 'animate-spin' : ''}`)} />
-          </Button>
-          <Button
-            size="icon"
-            variant="outline"
-            onClick={handleCopy}
-            disabled={copied}
-            className={cn('disabled:opacity-100')}
-          >
-            {copied ? (
-              <Check className={cn('h-4 w-4 text-green-600')} />
-            ) : (
-              <Copy className={cn('h-4 w-4')} />
-            )}
-          </Button>
-        </div>
-      </div>
-    </Card>
+    <div className={cn('flex flex-col gap-6')}>
+      {/** api key 발급/갱신 scope 설정 */}
+
+      <Card className={cn('p-6')}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={cn('mb-6 flex flex-col gap-4')}>
+            <div>
+              <p className={cn('mb-2 text-lg font-extrabold')}>API 권한 선택</p>
+              <p className={cn('text-muted-foreground text-sm')}>
+                API 키로 접근할 수 있는 권한을 선택하세요. 여러 개를 선택할 수 있습니다.
+              </p>
+            </div>
+            <div className="mb-4 space-y-6">
+              {availableKeyScope?.data?.data.map((category) => (
+                <div key={category.description}>
+                  <h3 className="mb-2 text-sm font-semibold">{category.title}</h3>
+                  <div className="space-y-2">
+                    {category.scopes.map((scope) => (
+                      <div
+                        key={scope.scope}
+                        className={`flex items-start gap-3 ${getIndentation(scope.scope)}`}
+                      >
+                        <Checkbox
+                          id={scope.scope}
+                          checked={selectedScopes.includes(scope.scope)}
+                          onCheckedChange={() => handleScopeToggle(scope.scope)}
+                        />
+                        <div className="flex-1">
+                          <label
+                            htmlFor={scope.scope}
+                            className={`cursor-pointer text-sm font-medium leading-none ${
+                              selectedScopes.includes(scope.scope) ? 'text-muted-foreground' : ''
+                            }`}
+                          >
+                            {scope.scope}
+                          </label>
+                          <p className="text-muted-foreground mt-1 text-sm">{scope.description}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <FormErrorMessage
+              error={Array.isArray(errors.scopes) ? errors.scopes[0] : errors.scopes}
+            />
+            <FormErrorMessage error={errors.description} />
+            <Button disabled={isCreatingApiKey || isUpdatingApiKey} size="lg" type="submit">
+              {buttonText}
+            </Button>
+          </div>
+        </form>
+      </Card>
+
+      {/** 현재발급한 api key 조회 */}
+      {apiKeyData?.data?.apiKey && (
+        <Card className={cn('p-6')}>
+          <div className="mb-4">
+            <p className="text-muted-foreground mb-2 text-sm">현재 발급된 API 키의 권한:</p>
+            <div className="flex flex-wrap gap-2">
+              {initialApiKeyData?.data.scopes.map((scope) => (
+                <p key={scope} className="bg-primary/10 text-primary rounded px-2 py-1 text-xs">
+                  {scope}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className={cn('flex items-center justify-between gap-4')}>
+            <div className={cn('min-w-0 flex-1')}>
+              <code className={cn('break-all font-mono text-sm')}>{apiKeyData?.data?.apiKey}</code>
+            </div>
+            <div className={cn('flex gap-2')}>
+              {/**
+              <Button
+              size="icon"
+              variant="outline"
+              onClick={handleCopy}
+              disabled={isUpdatingApiKey}
+              className={cn('disabled:opacity-100')}
+            >
+              <RefreshCw className={cn(`h-4 w-4 ${isUpdatingApiKey ? 'animate-spin' : ''}`)} />
+            </Button>
+             */}
+
+              <Button
+                size="icon"
+                variant="outline"
+                onClick={handleCopy}
+                disabled={copied}
+                className={cn('disabled:opacity-100')}
+              >
+                {copied ? (
+                  <Check className={cn('h-4 w-4 text-green-600')} />
+                ) : (
+                  <Copy className={cn('h-4 w-4')} />
+                )}
+              </Button>
+            </div>
+          </div>
+        </Card>
+      )}
+    </div>
   );
 };
 
