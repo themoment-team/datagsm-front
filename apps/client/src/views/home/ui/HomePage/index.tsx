eed12ecf44bd0396d9d9488ef77df96f4276ac74
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-12-19 15:16:19 +0900

    feat: renewable 삭제

--- apps/client/src/views/home/ui/HomePage/index.tsx
+++ apps/client/src/views/home/ui/HomePage/index.tsx
@@ -1,23 +1,17 @@
 import { cn } from '@repo/shared/utils';
 
-import { getApiKey, getApiKeyRenewable } from '@/views/home';
+import { getApiKey } from '@/views/home';
 import { ApiKeyCard, ApiKeyHeader } from '@/widgets/home';
 
 const HomePage = async () => {
-  const [initialApiKeyData, initialApiKeyRenewableData] = await Promise.all([
-    getApiKey(),
-    getApiKeyRenewable(),
-  ]);
+  const [initialApiKeyData] = await Promise.all([getApiKey()]);
 
   return (
     <div className={cn('bg-background h-[calc(100vh-4.0625rem)]')}>
       <main className={cn('container mx-auto px-4 py-16')}>
         <div className={cn('mx-auto max-w-2xl')}>
           <ApiKeyHeader />
-          <ApiKeyCard
-            initialApiKeyData={initialApiKeyData}
-            initialApiKeyRenewableData={initialApiKeyRenewableData}
-          />
+          <ApiKeyCard initialApiKeyData={initialApiKeyData} />
         </div>
       </main>
     </div>
