3d0348ce142b7c88feba524d9f301d6a68733357
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-12-23 15:46:44 +0900

    fix: 필요하지 않는 주석 제거

--- apps/client/src/entities/home/model/types.ts
+++ apps/client/src/entities/home/model/types.ts
@@ -9,7 +9,7 @@ export interface ApiKeyData {
 }
 
 export const ApiKeySchema = z.object({
-  scopes: z.array(z.string()).min(1, { message: '권한 범위를 최소 1개 이상 선택해주세요.' }), // 추후 수정
+  scopes: z.array(z.string()).min(1, { message: '권한 범위를 최소 1개 이상 선택해주세요.' }),
   description: z.string().min(1, { message: '설명을 입력해주세요.' }),
 });
 
