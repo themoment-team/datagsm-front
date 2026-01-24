785fee0edce573b5a5dca62b67169cbd2ac951a3
junjuny0227 <junjuny0227@gmail.com>
2026-01-25 02:07:06 +0900

    refactor: useGetClients 훅 위치를 views 레이어로 이동

--- apps/client/src/views/clients/index.ts
+++ apps/client/src/views/clients/index.ts
@@ -1,1 +1,2 @@
 export { default as ClientsPage } from './ui/ClientsPage';
+export * from './model/useGetClients';
--- apps/client/src/widgets/clients/index.ts
+++ apps/client/src/widgets/clients/index.ts
@@ -1,7 +1,6 @@
 export { default as ClientFormDialog } from './ui/ClientFormDialog';
 export { default as ClientList } from './ui/ClientList';
 export { default as ClientSuccessDialog } from './ui/ClientSuccessDialog';
-export * from './model/useGetClients';
 export * from './model/useCreateClient';
 export * from './model/useUpdateClient';
 export * from './model/useDeleteClient';
