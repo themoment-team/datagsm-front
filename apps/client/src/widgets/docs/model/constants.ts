9287ff44e2358f68a1ed0075e5c55fe9e99fd13b
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-02-04 22:15:54 +0900

    feat: OAuith 하위 구조 추가

--- apps/client/src/widgets/docs/model/constants.ts
+++ apps/client/src/widgets/docs/model/constants.ts
@@ -55,5 +55,17 @@ export const docsSections: DocsSection[] = [
     label: 'OAuth',
     href: '/docs/oauth',
     icon: User,
+    children: [
+      {
+        label: 'SDK',
+        href: '/docs/oauth/sdk',
+        children: [
+          {
+            label: 'JavaScript',
+            href: '/docs/oauth/sdk/javascript',
+          },
+        ],
+      },
+    ],
   },
 ];
