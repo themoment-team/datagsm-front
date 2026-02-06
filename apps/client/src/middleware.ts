fa53538d5e785945ead3df1c60d823b7ff97973b
junjuny0227 <junjuny0227@gmail.com>
2026-02-06 21:53:22 +0900

    fix: 아이콘 확장자 목록에 'ico' 추가

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -48,6 +48,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)',
   ],
 };
