c1f7859af29bdcb5bd34e4d4369cbbb5d7f58e65
junjuny0227 <junjuny0227@gmail.com>
2026-03-03 20:38:52 +0900

    feat: NODE_ENV 기반 docs URL 상수 추가

--- packages/shared/src/constants/navigation.ts
+++ packages/shared/src/constants/navigation.ts
@@ -1,8 +1,13 @@
+export const DOCS_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://docs.datagsm-front-client.vercel.app'
+    : 'http://localhost:3002';
+
 export const NAV_LINKS = {
   client: [
     { href: '/', label: '메인' },
     { href: '/clients', label: '클라이언트' },
-    { href: '/docs', label: '독스' }, // TODO: docs 애플리케이션 배포 후 링크 수정
+    { href: DOCS_URL, label: '독스' },
   ],
   admin: [
     { href: '/students', label: '학생' },
