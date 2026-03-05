a43b6d7fbb2690f6517536fc0ab9d6d52f69bc2c
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-05 18:23:29 +0900

    fix: 동아리 타입 -> 동아리 종류로 단어 변경

--- apps/admin/src/entities/club/model/schema.ts
+++ apps/admin/src/entities/club/model/schema.ts
@@ -9,7 +9,7 @@ export type ClubFilterType = z.infer<typeof ClubFilterSchema>;
 export const AddClubSchema = z.object({
   name: z.string().min(1, { message: '동아리명을 입력해주세요.' }),
   type: z.enum(['MAJOR_CLUB', 'JOB_CLUB', 'AUTONOMOUS_CLUB'], {
-    message: '동아리 타입을 선택해주세요.',
+    message: '동아리 종류를 선택해주세요.',
   }),
   leaderId: z.number({ message: '동아리 부장을 선택해주세요.' }).min(1),
   participants: z.array(z.number()).min(1, { message: '한 명 이상의 팀원을 선택해주세요.' }),
