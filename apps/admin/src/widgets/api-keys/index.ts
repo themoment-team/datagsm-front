e15bc81da4f423eb53926abbd5fccd498d5278cf
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-09 14:17:09 +0900

    feat: admin api key 생성

--- apps/admin/src/widgets/api-keys/index.ts
+++ apps/admin/src/widgets/api-keys/index.ts
@@ -1,2 +1,3 @@
 export { default as ApiKeyFilter } from './ui/ApiKeyFilter';
 export { default as ApiKeyList } from './ui/ApiKeyList';
+export { ApiKeyFormDialog } from '@repo/shared/ui';
--- apps/client/src/views/home/ui/HomePage/index.tsx
+++ apps/client/src/views/home/ui/HomePage/index.tsx
@@ -2,7 +2,7 @@ import { UserRoleType } from '@repo/shared/types';
 import { cn } from '@repo/shared/utils';
 
 import { getApiKey, getAvailableScope } from '@/views/home';
-import { ApiKeyDisplay, ApiKeyForm, ApiKeyHeader } from '@/widgets/home';
+import { ApiKeyDisplay, ApiKeyFormDialog, ApiKeyHeader } from '@/widgets/home';
 
 const HomePage = async () => {
   const userRole: UserRoleType = 'USER';
@@ -17,13 +17,13 @@ const HomePage = async () => {
       <main className={cn('container mx-auto px-4 py-16')}>
         <div className={cn('mx-auto max-w-2xl')}>
           <ApiKeyHeader />
-          <div className={cn('flex flex-col gap-6')}>
-            <ApiKeyForm
+          <div className={cn('flex flex-col gap-6 items-center')}>
+            <ApiKeyDisplay initialApiKeyData={initialApiKeyData} />
+            <ApiKeyFormDialog
               initialApiKeyData={initialApiKeyData}
               initialAvailableScope={initialAvailableScope}
               userRole={userRole}
             />
-            <ApiKeyDisplay initialApiKeyData={initialApiKeyData} />
           </div>
         </div>
       </main>
