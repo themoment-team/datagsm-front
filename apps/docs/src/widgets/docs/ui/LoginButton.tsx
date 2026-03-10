1e01f135e6a2fb91cfa6e542ab709c5a352f8afe
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-10 11:06:09 +0900

    feat: 스타일 가이드 수정 & 문구 추가

--- apps/docs/src/widgets/docs/ui/LoginButton.tsx
+++ apps/docs/src/widgets/docs/ui/LoginButton.tsx
@@ -9,7 +9,7 @@ export const LoginButton = ({ type = 'default', variant = 'white' }: LoginButton
   const isWhite = variant === 'white';
   const isBlack = variant === 'black';
   const isGray = variant === 'gray';
-  
+
   const styles = {
     height: type === 'icon' ? '70px' : '40px',
     width: type === 'icon' ? '70px' : 'auto',
@@ -17,23 +17,26 @@ export const LoginButton = ({ type = 'default', variant = 'white' }: LoginButton
     display: 'inline-flex',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: type === 'icon' ? '50%' : '1px',
+    borderRadius: type === 'icon' ? '50%' : '8px',
     cursor: 'pointer',
     fontWeight: '500',
     fontSize: '14px',
-    fontFamily: "Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif",
+    fontFamily:
+      "Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif",
     transition: 'all 0.2s ease-in-out',
     border: isGray ? '1px solid #E5E5E5' : 'none',
     backgroundColor: isWhite ? '#FFFFFF' : isBlack ? '#000000' : '#EFEFEF',
     color: isWhite ? '#000000' : isBlack ? '#FFFFFF' : '#000000',
   };
 
-  const logoSrc = (isWhite || isGray) ? '/images/docs/DG_black.png' : '/images/docs/DG_white.png';
+  const logoSrc = isWhite || isGray ? '/images/docs/DG_black.png' : '/images/docs/DG_white.png';
 
   return (
-    <div style={styles} className="hover:opacity-90 active:scale-[0.98] shadow-sm">
-      <img src={logoSrc} alt="logo" style={{ height: '16px'}} />
-      {type === 'default' && <span style={{ marginLeft: '16px', fontSize: '16px' }}>DataGSM으로 로그인</span>}
+    <div style={styles} className="shadow-sm hover:opacity-90 active:scale-[0.98]">
+      <img src={logoSrc} alt="logo" style={{ height: '16px' }} />
+      {type === 'default' && (
+        <span style={{ marginLeft: '16px', fontSize: '16px' }}>DataGSM으로 로그인</span>
+      )}
     </div>
   );
 };
