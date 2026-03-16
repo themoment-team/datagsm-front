b5473c513010b129213ed9327534a5f5cf1e72bb
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-16 17:17:22 +0900

    fix: 잘못된 컨벤션 수정

--- apps/client/src/entities/mypage/index.ts
+++ apps/client/src/entities/mypage/index.ts
@@ -1,7 +1,1 @@
-import { z } from 'zod';
-
-export const WithdrawalSchema = z.object({
-  password: z.string().min(1, '비밀번호를 입력해주세요.'),
-});
-
-export type WithdrawalFormType = z.infer<typeof WithdrawalSchema>;
+export * from './model/schema';
