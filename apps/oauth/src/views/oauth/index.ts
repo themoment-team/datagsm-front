6ebda51e1da16c5ed6dcef99501196f175b0187d
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-16 21:10:47 +0900

    fix: 경로 수정

--- apps/oauth/src/views/oauth/index.ts
+++ apps/oauth/src/views/oauth/index.ts
@@ -1,1 +1,1 @@
-export * from './ui/OAuthAuthorizePage';
+export * from './ui';
--- apps/oauth/src/views/oauth/ui/OAuthAuthorizePage/index.tsx
+++ apps/oauth/src/views/oauth/ui/OAuthAuthorizePage/index.tsx
@@ -4,10 +4,12 @@ import { cn } from '@repo/shared/utils';
 
 import { OAuthAuthorizeForm } from '@/widgets/oauth';
 
-export const OAuthAuthorizePage = () => {
+const OAuthAuthorizePage = () => {
   return (
     <div className={cn('bg-background flex min-h-screen items-center justify-center px-4')}>
       <OAuthAuthorizeForm />
     </div>
   );
 };
+
+export default OAuthAuthorizePage;
