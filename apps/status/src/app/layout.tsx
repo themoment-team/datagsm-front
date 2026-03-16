0142396d688f9040da2d9ca30dbf7117d5fd5500
junjuny0227 <junjuny0227@gmail.com>
2026-03-16 10:10:29 +0900

    feat: 상태 페이지 메타데이터 업데이트

--- apps/status/src/app/layout.tsx
+++ apps/status/src/app/layout.tsx
@@ -8,14 +8,14 @@ import type { Metadata } from 'next';
 import '@/shared/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'DataGSM',
-  description: '광주소프트웨어마이스터고등학교 OpenAPI & OAuth 플랫폼',
+  title: 'DataGSM Status',
+  description: '광주소프트웨어마이스터고등학교 OpenAPI & OAuth 플랫폼 상태 페이지',
   openGraph: {
-    title: 'DataGSM',
-    description: '광주소프트웨어마이스터고등학교 OpenAPI & OAuth 플랫폼',
-    url: 'https://datagsm-front-client.vercel.app/',
-    siteName: 'DataGSM',
-    images: 'https://datagsm-front-client.vercel.app/og-image.png',
+    title: 'DataGSM Status',
+    description: '광주소프트웨어마이스터고등학교 OpenAPI & OAuth 플랫폼 상태 페이지',
+    url: 'https://datagsm-front-status.vercel.app/',
+    siteName: 'DataGSM Status',
+    images: 'https://datagsm-front-status.vercel.app/og-image.png',
     type: 'website',
   },
 };
