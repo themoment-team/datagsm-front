a2fd0ce25881a6a3c29b550cd4f4f16da9a4b9f9
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-16 17:38:29 +0900

    feat: prefetch 적용

--- apps/client/src/app/myinfo/page.tsx
+++ apps/client/src/app/myinfo/page.tsx
@@ -1,7 +1,34 @@
+import { cookies } from 'next/headers';
+
+import { accountQueryKeys, accountUrl, get } from '@repo/shared/api';
+import { COOKIE_KEYS } from '@repo/shared/constants';
+import { MyAccountResponse } from '@repo/shared/types';
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+
 import MyInfoPage from '@/views/mypage/ui/MyInfoPage';
 
-const MyInfo = () => {
-  return <MyInfoPage />;
+const MyInfo = async () => {
+  const queryClient = new QueryClient();
+  const cookieStore = await cookies();
+  const token = cookieStore.get(COOKIE_KEYS.ACCESS_TOKEN)?.value;
+
+  if (token) {
+    await queryClient.prefetchQuery({
+      queryKey: accountQueryKeys.getMy(),
+      queryFn: () =>
+        get<MyAccountResponse>(accountUrl.getMy(), {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }),
+    });
+  }
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MyInfoPage />
+    </HydrationBoundary>
+  );
 };
 
 export default MyInfo;
