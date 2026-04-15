553b32cb4ea3335199fe9b2616585c7ef77b63e0
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-16 08:32:02 +0900

    fix: import 경로 수정

--- apps/client/src/widgets/myinfo/index.ts
+++ apps/client/src/widgets/myinfo/index.ts
@@ -1,6 +1,5 @@
 export * from './ui/ProfileInfo';
 export * from './ui/WithdrawalSection';
-export * from '@/shared/hooks/useGetMy';
 export * from './model/usePatchMyGithubId';
 export * from './model/usePatchMySpecialty';
 export * from './model/useWithdrawal';
